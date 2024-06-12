bccbc48c86c282090b34b84392c03418c5d754e4
fahrur <fahrurben@gmail.com>
2024-06-12 22:56:36 +0700

    Create admin group and category permission

--- nucommerce/admin.py
+++ nucommerce/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
-from nucommerce.models import Product, Category,Variant
+from nucommerce.models import CustomUser, Product, Category,Variant
 
 # Register your models here.
+admin.site.register(CustomUser)
 admin.site.register(Category)
 admin.site.register(Product)
 admin.site.register(Variant)
--- nucommerce/views/category_view.py
+++ nucommerce/views/category_view.py
@@ -1,10 +1,12 @@
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets
+from rest_framework.permissions import DjangoModelPermissions
 
 from nucommerce.models import Category
 from nucommerce.serializers import CategorySerializer
 
 
+
 class CategoryView(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [DjangoModelPermissions]
